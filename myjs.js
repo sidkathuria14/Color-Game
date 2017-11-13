b898008bad039f399a9968421bd56e15245b4a1f
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-11-13 20:12:07 +0530

    random color

--- myjs.js
+++ myjs.js
@@ -5,6 +5,13 @@
 
 //var doc = document.getElementById("msg");
 
+var arr = [];
+var squares = document.querySelectorAll(".square");
+var msg = document.querySelector("#msg");
+var color = document.querySelector("#color");
+var score = document.querySelector("#score");
+// msg.textContent = "Wrong!";
+// msg.textContent = "Right!";
 function randomColor(){
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
@@ -12,12 +19,31 @@ function randomColor(){
 
 	return "rgb(" + r + "," + g + "," + b + ")";
 }
+// document.onclick= function(event) {
+//     // Compensate for IE<9's non-standard event model
+//     //
+//     if (event===undefined) event= window.event;
+//     var target= 'target' in event? event.target : event.srcElement;
+
+//     alert('clicked on '+target.tagName);
+// };
+
+function randomNumber(){
+	var i = Math.floor(Math.random() * 6);
+	return i;
+}
+
+
 
 var squares = document.querySelectorAll(".square");
 for(var i= 0;i<6;++i ){
-squares[i].style.background = randomColor(); 
+ arr.push(randomColor());
+ // console.log(squares[i] );
+ squares[i].style.backgroundColor = arr[i];
+
+// = arr[i];
 }
 
-document.getElementById("msg").textContent = "try again";
+// document.getElementById("msg").textContent = "try again";
 
 
