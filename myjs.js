15039661ca60ea90c0398b436949ccabf3ac6dfb
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-11-18 01:16:08 +0530

    added some more css

--- myjs.js
+++ myjs.js
@@ -1,53 +1,85 @@
-// alert("hello");
-
-// var clickedColor = this.style.backgroundColor;
-// alert(clickedColor);
-
-//var doc = document.getElementById("msg");
-
 var arr = [];
-// var selectedAnswer = ;
-// var correctAnswer = ;
 var squares = document.querySelectorAll(".square");
-var msg = document.querySelector("#msg");
-var color = document.querySelector("#color");
-var score = document.querySelector("#score");
-// msg.textContent = "Wrong!";
-// msg.textContent = "Right!";
+var score = 0;
+
+arr = fill();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var scoreDisplay = document.querySelector("#score");
+var head = document.querySelector("h1");
+
+ var pickedColor = selectedColor();
+  colorDisplay.textContent = pickedColor;
+ console.log(pickedColor);
+
+for(var i= 0;i<6;++i ){
+  
+   squares[i].style.background = arr[i];
+   squares[i].addEventListener("click",function(){
+    var clickedcolor = this.style.background;
+    console.log(clickedcolor);
+    console.log(pickedColor);
+    if(clickedcolor === pickedColor){
+      messageDisplay.textContent = "correct!";
+      score  = score+1;
+      changeColor();
+    }
+    else {
+    	this.style.background = "#132838"
+// this.style.opacity += 0.1;
+    	messageDisplay.textContent = "try again!!";
+scoreDisplay.textContent = "Your Score is : " + score; 
+}
+  
+  });
+}
+
+function changeColor(){
+	for(var i=0;i<6;i++){
+		squares[i].style.background = pickedColor;
+	}
+  // colorDisplay.style.color  = pickedColor;
+head.style.background = pickedColor;
+}
+
+function fill(){
+  var arr = [];
+  for(var i=0;i<6;i++){
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
 function randomColor(){
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
 
-	return "rgb(" + r + "," + g + "," + b + ")";
-}
-// document.onclick= function(event) {
-//     // Compensate for IE<9's non-standard event model
-//     //
-//     if (event===undefined) event= window.event;
-//     var target= 'target' in event? event.target : event.srcElement;
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 
-//     alert('clicked on '+target.tagName);
-// };
+}
+
 
 function randomNumber(){
 	var i = Math.floor(Math.random() * 6);
+
 	return i;
 }
+function selectedColor(){
 
-
-
-var squares = document.getElementsByClassName(".square");
-for(var i= 0;i<6;++i ){
- arr.push(randomColor());
-  // console.log(squares[i] );
- squares[i].style.backgroundColor = arr[i];
-
-// = arr[i];
+	var pickedcolor = arr[randomNumber()];
+console.log(pickedcolor);
+  return pickedcolor;
 }
-// var correctAnswer = arr[randomNumber];
-// color.textContent = arr[correctAnswer];
 
-// document.getElementById("msg").textContent = "try again";
+
+
+//var squares = document.querySelectorAll(".square");
+
+
+// var correctAnswer = arr[randomNumber];
+// color.textContent = correctAnswer;
+// console.log(correctAnswer);
+
 
 
